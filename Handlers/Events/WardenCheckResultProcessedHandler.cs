b20a5a3ae02511076e20d931941906d5c046f2d8
Piotr Gankiewicz <spetz90@gmail.com>
2016-11-03 18:43:12 +0100

    Init

---
+++ Handlers/Events/WardenCheckResultProcessedHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Warden.Common.Events;
+using Warden.Common.Events.Wardens;
+
+namespace Warden.Services.Stats.Handlers.Events
+{
+    public class WardenCheckResultProcessedHandler : IEventHandler<WardenCheckResultProcessed>
+    {
+        public async Task HandleAsync(WardenCheckResultProcessed @event)
+        {
+            Console.WriteLine("Updating stats...");
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Warden.Common.Events.Wardens;
+using Warden.Common.Host;
+using Warden.Services.Stats.Framework;
+
+namespace Warden.Services.Stats
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebServiceHost
+                .Create<Startup>(port: 10005)
+                .UseAutofac(Bootstrapper.LifetimeScope)
+                .UseRabbitMq(queueName: typeof(Program).Namespace)
+                .SubscribeToEvent<WardenCheckResultProcessed>()
+                .Build()
+                .Run();
+        }
+    }
+}
