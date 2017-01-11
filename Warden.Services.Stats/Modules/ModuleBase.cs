cf3ce3ebe15aeb0c0915bbc1eeb4b82a22a5d152
spetz <spetz90@gmail.com>
2017-01-11 13:34:04 +0100

    Secured services

--- Warden.Services.Stats/Modules/ModuleBase.cs
+++ Warden.Services.Stats/Modules/ModuleBase.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Nancy.Security;
 using Warden.Common.Nancy;
 
 namespace Warden.Services.Stats.Modules
@@ -16,7 +17,7 @@ namespace Warden.Services.Stats.Modules
         { 
             if (requireAuthentication)
             {
-                // this.RequiresAuthentication();
+                this.RequiresAuthentication();
             }
         }
     }
